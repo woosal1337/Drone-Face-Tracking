8bdca683159227b9bce639f48bd064330c8dda28
Vusal Ismayilov <69302858+woosal1337@users.noreply.github.com>
2021-01-05 11:03:33 +0300

    2.0

---
+++ faceTracking.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+# Finding the face phase
+def findFace(img):
+    
+
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    _, img = cap.read()
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
